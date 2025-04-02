67027006b075ce059a77b5ba48e110b69cfab24a
ZedNotic <57990129+xZed-atShaon@users.noreply.github.com>
2025-04-02 17:12:53 +0530

    add dictionary lookup in  nutrition.py

--- Loops/nutrition/nutrition.py
+++ Loops/nutrition/nutrition.py
@@ -1,31 +1,35 @@
-cal_of_fruits =[
-    {"fruit":"apple", "calorie":"130"},
-    {"fruit":"avocado", "calorie":"50"},
-    {"fruit":"banana", "calorie":"110"},
-    {"fruit":"cantaloupe", "calorie":"50"},
-    {"fruit":"grapefruit", "calorie":"60"},
-    {"fruit":"grapes", "calorie":"90"},
-    {"fruit":"honeydew melon", "calorie":"50"},
-    {"fruit":"kiwifruit", "calorie":"90"},
-    {"fruit":"lemon", "calorie":"15"},
-    {"fruit":"lime", "calorie":"20"},
-    {"fruit":"nectarine", "calorie":"60"},
-    {"fruit":"orange", "calorie":"80"},
-    {"fruit":"peach", "calorie":"60"},
-    {"fruit":"pear", "calorie":"100"},
-    {"fruit":"pineapple", "calorie":"50"},
-    {"fruit":"plums", "calorie":"70"},
-    {"fruit":"strawberries", "calorie":"50"},
-    {"fruit":"sweet cherries", "calorie":"100"},
-    {"fruit":"tangerine", "calorie":"50"},
-    {"fruit":"watermelon", "calorie":"80"},
-]
+cal_of_fruits = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
+}
   
-name = input("Item: ")
+name = input("Item: ").lower()  # Convert input to lowercase once
 
-for fruits in cal_of_fruits:
-	if name.lower() == fruits["fruit"]:
-		print("Calories:", fruits["calorie"])
-	else:
-		None
+if name in cal_of_fruits:
+    print("Calories:", cal_of_fruits[name])  # Direct dictionary lookup
+
+# Your old code looping through the list
+# for fruits in cal_of_fruits:
+	# if name == fruits["fruit"]:
+		# print("Calories:", fruits["calorie"])
+	# else:
+		# None
 		# print("", sep="", end="")
